30fb0b5e873585d9999ba7750c4914721331c4dc
Lukas Kosina <lukas@kosinovi.eu>
2018-11-29 12:46:19 +0100

    Initialized project

---
+++ src/main/java/cz/kosina/databasebrowser/DatabaseBrowserApplication.java
@@ -0,0 +1,12 @@
+package cz.kosina.databasebrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseBrowserApplication.class, args);
+	}
+}
